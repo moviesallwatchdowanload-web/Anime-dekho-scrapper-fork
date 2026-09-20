c35a898400eedc08f2bcddc03a19dc9d2fe8fd57
Xeonmovies.github.io <moviesallwatchdowanload@gmail.com>
2026-09-20 23:58:29 +0530

    Create build.gradle.kts

---
+++ AnimeDekhoProvider/build.gradle.kts
@@ -0,0 +1,21 @@
+// Use an integer for version numbers
+version = 1
+
+cloudstream {
+    description = "AnimeDekho - Hindi Dubbed Anime Movies & Series"
+    authors = listOf("moviesallwatchdowanload-web")
+
+    /**
+     * Status int as one of the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta-only
+     **/
+    status = 1
+
+    tvTypes = listOf("Movie", "TvSeries", "Anime", "AsianDrama")
+    iconUrl = "https://animedekho.app/favicon.ico"
+
+    isCrossPlatform = true
+}
